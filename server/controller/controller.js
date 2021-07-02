c21c067a588d7c24b93f3a4c8bd43f5cb743fed2
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-07-02 14:42:49 +0530

    successfully create RestAPI

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req, res) => {
+
+}
+
+// retrieve and erturn all users/ retrieve and return a single user
+exports.find = (req, res) => {
+
+}
+
+// update a user by id
+exports.update = (req, res) =>{
+
+}
+
+// delete a user with user id in the request
+exports.delete = (req, res) => {
+    
+}
