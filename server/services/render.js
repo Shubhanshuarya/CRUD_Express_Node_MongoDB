d792941786c9a6a21754b2f80783ebacb1bf818e
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-07-02 15:45:56 +0530

    successfully fetch the data from mongodb database

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,19 @@
+const axios = require('axios');
+
+
+
 exports.homeRoutes = (req, res) => {
-    res.render('index');
+    axios.get('http://localhost:3000/api/users')
+    .then(function (response) {
+        console.log(response);
+        res.render('index', {users: response.data});
+    })
+    .catch(
+        err =>{
+            res.send(err);
+        }
+    )
+    
 }
 
 exports.add_user = (req, res) => {
