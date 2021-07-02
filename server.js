a633604f420842f8993b3475bb5ed48cc2757c70
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-07-02 12:54:41 +0530

    Setting up the ejs module with an example

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -10,8 +12,22 @@ const PORT = process.env.PORT || 4000;
 //log requests
 app.use(morgan('tiny'));
 
+// Parse request to body parser
+app.use(bodyParser.urlencoded({ extended: true}))
+
+//set view engine
+app.set('view engine', "ejs");
+// app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+// Load Assets
+app.use("\css", express.static(path.resolve(__dirname, "assets/css")));
+// css/style.css
+app.use('\js', express.static(path.resolve(__dirname, "assets/js")));
+app.use('\img', express.static(path.resolve(__dirname, "assets/img")));
+
 app.get('/', (req, res) => {
-    res.send("Crud Application");
+    // res.send("Crud Application");
+    res.render('index');
 })
 
 app.listen(PORT, ()=>{
