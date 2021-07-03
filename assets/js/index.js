58fb9bfbe9fc2aafe82877d43fa8e7bc27a5f911
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-07-03 12:05:16 +0530

    trying to solve link problem

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ $("#update_user").submit(function(event){
     })
 
     var request = {
-        "url" : `http://crudappexpress786.herokuapp.com/api/users/${data.id}`,
+        "url" : `https://crudappexpress786.herokuapp.com/api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -32,7 +32,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://crudappexpress786.herokuapp.com/api/users/${id}`,
+            "url" : `https://crudappexpress786.herokuapp.com/api/users/${id}`,
             "method" : "DELETE"
         }
 
